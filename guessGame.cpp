01e2a61fa6d498f4cbe31f47e79b6e6a049150a3
Student-2024123 <morankardipesh@gmail.com>
2024-07-15 17:50:23 +0530

    A guessing game

---
+++ guessGame.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int main() {
+    int myNumber = rand() % 100 + 1;  // Generate random number between 1 and 100
+    int userNumber = 0;
+    int attempts = 0;
+
+    do {
+        cout << "Guess my number (1-100): ";
+        cin >> userNumber;
+        attempts++;
+        
+        if (userNumber == myNumber) {
+            cout << "WOOHOO .. CORRECT NUMBER!!!" << endl;
+            break;
+        } else if (userNumber > myNumber) {
+            cout << "Your number is too large" << endl;
+        } else {
+            cout << "Your number is too small" << endl;
+        }
+    } while (userNumber >= 0);
+
+    cout << "My number was: " << myNumber << endl;
+    cout << "You took " << attempts << " attempts to guess!" << endl;
+
+    return 0;
+}
+
+
